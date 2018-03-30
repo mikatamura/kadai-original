0c4982f0a6c0338a7db59b212dd4dff9c65a7e3a
Mika Tamura <m.yu.oor.rabis10ve@gmail.com>
2018-03-30 07:48:00 +0000

    post

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,28 @@
+class MoviesController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
+
+  def create
+    @movie = current_user.movies.build(movie_params)
+    if @movie.save
+      flash[:success] = 'コメントを投稿しました。'
+      redirect_to root_url
+    else
+      @movies = current_user.movies.order('created_at DESC').page(params[:page])
+      flash.now[:danger] = 'コメントの投稿に失敗しました。'
+      render 'toppages/index'
+    end
+  end
+
+  def destroy
+    @movie.destroy
+    flash[:success] = 'コメントを削除しました。'
+    redirect_back(fallback_location: root_path)
+  end
+  
+  private
+
+  def movie_params
+    params.require(:movie).permit(:content)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180330003031) do
+ActiveRecord::Schema.define(version: 20180330063611) do
+
+  create_table "movies", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_movies_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
@@ -20,4 +28,5 @@ ActiveRecord::Schema.define(version: 20180330003031) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "movies", "users"
 end
